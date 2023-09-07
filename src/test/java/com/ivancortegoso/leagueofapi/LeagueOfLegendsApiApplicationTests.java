e3a182b7e4b09a60deca2b9713fef4416713303e
ivancortegoso <ivancortegoso@hotmail.com>
2023-09-07 09:01:41 +0200

    initial commit

---
+++ src/test/java/com/ivancortegoso/leagueofapi/LeagueOfLegendsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivancortegoso.leagueofapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueOfLegendsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
